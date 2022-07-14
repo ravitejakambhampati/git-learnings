66b29ccf19d5f505afcb7be3d5e682e0fe2e3be4
Avinash <chinnabattuni.avinash@byjus.com>
2022-07-14 12:14:35 +0530

    Done with the given coding part in Python

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
 #given an array of numbers find any two numbers , whose sum equal to given number
-a=[1,2,3,4,5]
-n=7
+def pairs(a, s, n):
+    for i in range (0,s-1):
+        for j in range(i+1, s):
+            if(a[i]+a[j]==n):
+                print(f" pair is : ({a[i],a[j]}) ")
+
+                return 1
+    return 0
+if __name__ == "__main__":
+    a=[1,2,3,4,5]
+    n=7
+    s=len(a)
+    
